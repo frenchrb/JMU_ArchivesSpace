0f333466c62ac3787df2e48a17876de0f70fdfa1
Rebecca French <frenchrb@jmu.edu>
2018-09-11 14:03:19 -0400

    Add out_dir arg to functions

--- spaceport.py
+++ spaceport.py
@@ -6,13 +6,14 @@ import subprocess
 import configparser
 import os
 import requests
+from pathlib import Path
 
 #read config file
 config = configparser.ConfigParser()
 config.read('local_settings.ini')
 
 
-def export_ead(list, id_dict):
+def export_ead(list, out_dir, id_dict):
     #export all collections listed in input from ArchivesSpace
     
     # URL parameters dictionary, used to manage common URL patterns
@@ -26,6 +27,9 @@ def export_ead(list, id_dict):
     
     headers = {'X-ArchivesSpace-Session':session}
     export_options = '?numbered_cs=true&include_daos=true&include_unpublished=true'
+    
+    destination = out_dir / 'Aspace_EADs'
+    Path.mkdir(destination)
     
     with open(list) as f:
         for line in f:
@@ -48,42 +52,40 @@ def export_ead(list, id_dict):
             
             coll_num = resource['id_0'].replace(' ','')
             if 'ead_id' in resource:
-                print(resource['ead_id'])
+                #print(resource['ead_id'])
                 id_dict[coll_num] = resource['ead_id']
             else:
                 id_dict[coll_num] = ''
             
-            destination = 'Aspace_EADs/'
-            outfile = open(destination + coll_num + '.xml', 'w', encoding = 'utf-8')
-            #outfile.write(ead.encode('utf-8'))  #py2
-            outfile.write(ead)
-            outfile.close
+            out_filename = Path(coll_num).with_suffix('.xml')
+            outfile = destination / out_filename
+            outfile.write_text(ead, encoding = 'utf-8')
     print('EAD exports completed')
     return id_dict
 
-def vaheritage(inputFile, id_dict):
-    #run Aspace2VaHeritage.xsl transformation with Saxon
+def vaheritage(in_file, out_dir, id_dict):
+    source = out_dir / 'Aspace_EADs' / in_file
     
-    if id_dict[os.path.splitext(inputFile)[0]]:
-        outname = id_dict[os.path.splitext(inputFile)[0]]
+    #use vihart# as filename if it exists; otherwise use collection number
+    if id_dict[in_file.stem]:
+        out_file = (out_dir / 'Va_Heritage' / id_dict[in_file.stem]).with_suffix('.xml')
     else:
-        outname = os.path.splitext(inputFile)[0]
-    outpath = 'VaHeritage/'+outname+'.xml'
-    #subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:Aspace_EADs/'+inputFile, '-xsl:Aspace2VaHeritage.xsl', '-o:VaHeritage/'+out_name+'.xml'])
-    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:Aspace_EADs/'+inputFile, '-xsl:Aspace2VaHeritage.xsl', '-o:'+outpath])
+        out_file = out_dir / 'Va_Heritage' / in_file
     
+    #run Aspace2VaHeritage.xsl transformation with Saxon
+    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(source.resolve()), '-xsl:Aspace2VaHeritage.xsl', '-o:'+str(out_file.resolve())])
     
-    #TODO try to make the non-XSLT changes
-    with open('VaHeritage/'+outname+'.xml', 'r') as file:
+    #non-XSLT changes
+    with open(out_file, 'r') as file:
         filedata = file.read()
     filedata = filedata.replace('<extref xmlns=', '<extref xmlns:xlink=')
     filedata = filedata.replace('<extptr xmlns=', '<extptr xmlns:xlink=')
-    with open(outpath, 'w') as file:
+    with open(out_file, 'w') as file:
         file.write(filedata)
     
-    return 'Virginia Heritage transformation executed (' + outname + ')'
+    return 'Virginia Heritage transformation executed (' + out_file.stem + ')'
 
-def marcxml(inputFile):
+def marcxml(in_file, out_dir):
     #run Aspace2MARCXML.xsl transformation with Saxon
     
     #encoding troubleshooting
@@ -95,17 +97,23 @@ def marcxml(inputFile):
     #print()
     #encoding troubleshooting
     
-    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:Aspace_EADs/'+inputFile, '-xsl:Aspace2MARCXML.xsl', '-o:MARCXML/'+os.path.splitext(inputFile)[0]+'_MARC.xml'])
+    source = out_dir / 'Aspace_EADs' / in_file
+    out_file = (out_dir / 'MARCXML' / (in_file.stem + '_MARC')).with_suffix('.xml')
+    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(source.resolve()), '-xsl:Aspace2MARCXML.xsl', '-o:'+str(out_file.resolve())])
     return 'MARCXML transformation executed'
 
-def html(inputFile):
+def html(in_file, out_dir):
     #run Aspace2HTML.xsl transformation with Saxon
-    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:Aspace_EADs/'+inputFile, '-xsl:Aspace2HTML.xsl', '-o:HTML/'+os.path.splitext(inputFile)[0]+'.html'])
+    source = out_dir / 'Aspace_EADs' / in_file
+    out_file = out_dir / 'HTML' / in_file.with_suffix('.html')
+    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(source.resolve()), '-xsl:Aspace2HTML.xsl', '-o:'+str(out_file.resolve())])
     return 'HTML transformation executed'
 
-def html_abstract(inputFile):
+def html_abstract(in_file, out_dir):
     #run Aspace2HTMLabstract.xsl transformation with Saxon
-    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:Aspace_EADs/'+inputFile, '-xsl:Aspace2HTMLabstract.xsl', '-o:HTML_abstract/'+os.path.splitext(inputFile)[0]+'abstract.html'])
+    source = out_dir / 'Aspace_EADs' / in_file
+    out_file = (out_dir / 'HTML_abstract' / (in_file.stem + 'abstract')).with_suffix('.html')
+    subprocess.call(['java', '-jar', config['Saxon']['saxon_path']+'saxon9he.jar', '-s:'+str(source.resolve()), '-xsl:Aspace2HTMLabstract.xsl', '-o:'+str(out_file.resolve())])
     return 'HTML abstract transformation executed'
 
 def cleanup(filename):
@@ -117,6 +125,7 @@ def main(arglist):
     #print(arglist)
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='text file of collection numbers')
+    parser.add_argument('output', help='save directory')
     parser.add_argument('--vaheritage', help='run ASpace2VaHeritage.xsl', action='store_true')
     parser.add_argument('--marcxml', help='run ASpace2MARCXML.xsl', action='store_true')
     parser.add_argument('--html', help='run ASpace2HTML.xsl', action='store_true')
@@ -128,27 +137,31 @@ def main(arglist):
     coll_list = arglist[0]
     #print(coll_list)
     
+    save_dir = Path(arglist[1])
+    out_dir = save_dir / 'Output'
+    Path.mkdir(out_dir) #should first check if exists
+    
     sc_vihart = {}
-    sc_vihart = export_ead(coll_list, sc_vihart)
+    sc_vihart = export_ead(coll_list, out_dir, sc_vihart)
     #print(sc_vihart)
     print()
     
     with open(coll_list) as f:
         for line in f:
-            filename = line.replace(' ', '').rstrip() + '.xml'
-            if filename in os.listdir('Aspace_EADs'):
+            filename = Path(line.replace(' ', '').rstrip()).with_suffix('.xml')
+            if filename.name in os.listdir(out_dir / 'Aspace_EADs'):
                 if args.vaheritage or args.marcxml or args.html or args.htmlabs or args.retainexport == False:
                     print(filename)
                 if args.vaheritage:
-                    print('    ' + vaheritage(filename, sc_vihart))
+                    print('    ' + vaheritage(filename, out_dir, sc_vihart))
                 if args.marcxml:
-                    print('    ' + marcxml(filename))
+                    print('    ' + marcxml(filename, out_dir))
                 if args.html:
-                    print('    ' + html(filename))
+                    print('    ' + html(filename, out_dir))
                 if args.htmlabs:
-                    print('    ' + html_abstract(filename))
+                    print('    ' + html_abstract(filename, out_dir))
                 if args.retainexport == False:
-                    print('    ' + cleanup('Aspace_EADs/' + filename))
+                    print('    ' + cleanup(out_dir / 'Aspace_EADs' / filename))
                 print()
     
 if __name__ == '__main__':
